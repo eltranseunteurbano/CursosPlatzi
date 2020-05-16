15e9541d67d469aec3623b5206fd88c042f59db5
Jaime Burbano <burbanojaime98@gmail.com>
2020-05-16 18:57:04 -0500

    first commit

---
+++ Fundamentos de Bases de Datos/Archivos mySQL/participants.sql
@@ -0,0 +1,24 @@
+SELECT * FROM projects.participants;
+
+INSERT INTO projects.participants (id, name, linkedin, website, behance, dribbble, instagram) VALUES (1, "Jaime Burbano", "eltranseunteurbano", "www.jaimeburbano.com", "eltranseunteurbano", "eltranseunteurbano", "eltranseunteurbano");
+INSERT INTO projects.participants (id, name, linkedin, website, behance, instagram, medium) VALUES (2, "Camila Morales", "mcamilamorales", "https://camilamorales.webflow.io/", "mcamilamorales", "imdmcm", "mcamilamorales");
+INSERT INTO projects.participants (id, name, linkedin, website, behance, instagram, medium) VALUES (3, "Valentina Moreno", "vmorenodesign", "https://readymag.com/u47243143/VMDesign/", "ValentinaMoreno", "valen050298", "valentina02051998");
+INSERT INTO projects.participants (id, name, linkedin, website, behance, instagram) VALUES (4, "Isabella Sandoval", "sandovalvillarreal", "www.isabellasandoval.com", "IsabellaSandoval", "isabella.sandoval.12");
+INSERT INTO projects.participants (id, name, linkedin, website, behance) VALUES (5, "Jhonathan Rios", "jhonatanrios", "https://readymag.com/u73892456/jhonatanrios/", "jriosdesigner");
+INSERT INTO projects.participants (id, name, behance, linkedin, medium) VALUES (6, "Mauricio Botero", "KeonsinKryte", "keonsinkryte", "keonsinkryte");
+INSERT INTO projects.participants (id, name, behance, instagram) VALUES (7, "Carolina Escobar", "caroeb", "mce_photo");
+INSERT INTO projects.participants (id, name, behance, instagram) VALUES (8, "Oscar Hoyos", "oscarht597", "oscarht597");
+INSERT INTO projects.participants (id, name, behance, instagram) VALUES (9, "Lina Benaver", "buenaver", "buenaver_");
+INSERT INTO projects.participants (id, name, behance, linkedin) VALUES (10, "Estefanía Montaña", "emontbui", "emonbui");
+INSERT INTO projects.participants (id, name, behance) VALUES (11, "Camila Polania", "milaPolania");
+INSERT INTO projects.participants (id, name, behance) VALUES (12, "Catalina Reyes", "catalalina2347");
+
+INSERT INTO projects.participants (id, name)  VALUES (13, "María del Mar Martínez");
+INSERT INTO projects.participants (id, name)  VALUES (14, "Daniela Angel");
+INSERT INTO projects.participants (id, name)  VALUES (15, "Daniela García");
+INSERT INTO projects.participants (id, name)  VALUES (16, "Alejandro Salazar");
+
+SELECT * FROM projects.participants;
+
+
+
---
+++ README.md.txt
@@ -0,0 +1,7 @@
+# CURSOS PLATZI
+
+Archivos de estudio de los diferenes cursos de Platzi y sus respectivos certificados
+
+### Cursos
+
+- [Fundamentos de bases de datos ](https://platzi.com/clases/bd/ "Fundamentos de bases de datos ")

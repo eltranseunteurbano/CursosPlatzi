e407b61267348d123a1910851e8c64a686bcbb33
Jaime Burbano <burbanojaime98@gmail.com>
2020-05-26 13:20:56 -0500

    renderizando listas

--- Cursos/VueJs/AtributosDinamicos/index.js
+++ Cursos/VueJs/AtributosDinamicos/index.js
@@ -5,7 +5,17 @@ new Vue({
         return{
             name: 'Bitcoin',
             img: 'http://cryptologos.cc/logos/bitcoin-btc-logo.png',
-            changePercent: 0
+            changePercent: 0,
+            prices: [8400, 7900, 8200, 9000, 9400, 10000,10200],
+            pricesWithDays: [
+                { day: 'Lunes', value: 8400 },
+                { day: 'Martes', value: 7900 },
+                { day: 'Miercoles', value: 8200 },
+                { day: 'Jueves', value: 9000 },
+                { day: 'Viernes', value: 9400 },
+                { day: 'Sabado', value: 10000 },
+                { day: 'Domingo', value: 10200 },
+            ]
         }
     }
 })

93b60ed06e4254db315648e4c9063d116c839eb4
Jaime Burbano <burbanojaime98@gmail.com>
2020-05-28 20:16:33 -0500

    reto de suma de cursos de platzi

---
+++ Cursos/VueJs/CursosPlatzi/index.js
@@ -0,0 +1,37 @@
+new Vue({
+    el: '#app',
+
+    data () {
+        return{
+            courses: [],
+            title: "",
+            hour: "",
+        }
+    },
+    watch: {
+
+       
+    },
+    computed: {
+       totalTime(){
+           let total = 0;
+           if(this.courses.length > 0){
+                this.courses.forEach(element => {
+                   total += parseInt(element.hour)
+               });
+            }
+            return total;
+       }
+    },
+    methods: {
+        addCourse (){
+            if ( this.title != "" && this.hour != ""){
+                this.courses.push({
+                    'title': this.title,
+                    'hour': this.hour });
+                this.hour = "";
+                this.title = "";
+            }
+        }
+    }
+});

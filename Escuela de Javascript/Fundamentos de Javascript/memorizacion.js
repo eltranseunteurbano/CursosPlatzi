e113993dff9dbf1ad643dc18316851d2aaf529f4
Jaime Burbano <burbanojaime98@gmail.com>
2020-06-21 18:06:50 -0500

    recursividad

---
+++ Escuela de Javascript/Fundamentos de Javascript/memorizacion.js
@@ -0,0 +1,12 @@
+function factorial(n) {
+
+    if( !this.cache ) this.cache = {}
+     
+    if (this.cache[n]) return this.cache[n]
+
+    if ( n == 1 ) return 1
+
+    this.cache[ n ] = n * factorial( n - 1 )
+    console.log(this.cache[n])
+    return this.cache[ n ]
+}

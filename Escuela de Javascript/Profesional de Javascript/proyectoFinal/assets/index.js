63125df5298835770379682951e6f21a0954b363
Jaime Burbano <burbanojaime98@gmail.com>
2020-07-01 11:49:08 -0500

    this objecto en javascript

--- Escuela de Javascript/Profesional de Javascript/proyectoFinal/assets/index.js
+++ Escuela de Javascript/Profesional de Javascript/proyectoFinal/assets/index.js
@@ -1,7 +1,7 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({ el: video, plugins: /*[new AutoPlay()] */[]});
 
 const button = document.querySelector('button');
 button.onclick = () => player.togglePlay();

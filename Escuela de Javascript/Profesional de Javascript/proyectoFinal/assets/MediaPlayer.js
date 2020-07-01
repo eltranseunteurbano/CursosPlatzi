8801d70aa7e5df2f549d53f395bc4a4e50c01806
Jaime Burbano <burbanojaime98@gmail.com>
2020-07-01 11:23:22 -0500

    Plugin autoplayer

--- Escuela de Javascript/Profesional de Javascript/proyectoFinal/assets/MediaPlayer.js
+++ Escuela de Javascript/Profesional de Javascript/proyectoFinal/assets/MediaPlayer.js
@@ -1,5 +1,14 @@
 function MediaPlayer(config) {
   this.media = config.el;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
+}
+
+MediaPlayer.prototype._initPlugins = function() {
+  this.plugins.forEach(plugin => {
+    plugin.run(this);
+  })
 }
 
 MediaPlayer.prototype.play = function() {
@@ -9,6 +18,14 @@ MediaPlayer.prototype.play = function() {
 MediaPlayer.prototype.pause = function() {
   this.media.pause();
 };
+
+MediaPlayer.prototype.mute = function() {
+  this.media.muted = true;
+}
+
+MediaPlayer.prototype.unmute = function() {
+  this.media.muted = false;
+}
 
 MediaPlayer.prototype.togglePlay = function() {
   if (this.media.paused) {

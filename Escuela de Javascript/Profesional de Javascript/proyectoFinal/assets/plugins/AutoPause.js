d33ce6b23a5fcc69cc817c14d2e744b5188f589f
Jaime Burbano <burbanojaime98@gmail.com>
2020-07-02 10:31:43 -0500

    vibility change

--- Escuela de Javascript/Profesional de Javascript/proyectoFinal/assets/plugins/AutoPause.js
+++ Escuela de Javascript/Profesional de Javascript/proyectoFinal/assets/plugins/AutoPause.js
@@ -2,12 +2,15 @@ class AutoPause {
   constructor() {
     this.threshold = 0.25;
     this.handleIntersection = this.handleIntersection.bind(this)
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
   }
   run(player) {
     this.player = player;
     const config = { threshold: this.threshold,}
     const observer = new IntersectionObserver(this.handleIntersection, config)
     observer.observe(this.player.media)
+
+    document.addEventListener("visibilitychange", this.handleVisibilityChange)
   }
 
   handleIntersection(entries) {
@@ -15,6 +18,11 @@ class AutoPause {
     const isVisible = entry.intersectionRatio >= this.threshold;
     isVisible ? this.player.play() : this.player.pause();
     console.log(entry, isVisible)
+  }
+
+  handleVisibilityChange() {
+    const isVisible = document.visibilityState === "visible"
+    isVisible ? this.player.play() : this.player.pause();
   }
 }
 export default AutoPause;

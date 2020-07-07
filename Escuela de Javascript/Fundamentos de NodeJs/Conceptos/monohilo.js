12e9126a1bfc6e64518c66311345452e317ca436
Jaime Burbano <burbanojaime98@gmail.com>
2020-07-07 15:33:31 -0500

    Fundamentos de Node Js

---
+++ Escuela de Javascript/Fundamentos de NodeJs/Conceptos/monohilo.js
@@ -0,0 +1,11 @@
+console.log('Hola mundo');
+
+let i = 0;
+setInterval(()=>{
+  console.log(i);
+  i++
+
+  if(i === 5) {
+    var a = 3 + z;
+  }
+}, 1000);
--- Escuela de Javascript/React Router y Redux/videoplatzi/src/components/Search.jsx
+++ Escuela de Javascript/React Router y Redux/videoplatzi/src/components/Search.jsx
@@ -5,8 +5,7 @@ import '../assets/styles/components/Search.scss';
 const Search = ({ isHome }) => {
   const inputStyle = classNames('input', {
     isHome,
-	});
-	
+  });
   return (
     <section className='main'>
       <h2 className='main__title'>¿Qué quieres ver hoy?</h2>

c2497f349864de20d216fa8abf5875f83b37d5d7
Jaime Burbano <burbanojaime98@gmail.com>
2020-07-14 18:45:26 -0500

    File syste my globals, modulos del corte

---
+++ Escuela de Javascript/Fundamentos de NodeJs/modulos/fs.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+function leer(ruta, callback){
+  fs.readFile(ruta, (error, data) => {
+    //Archivo leido
+    callback(data.toString())
+  })  
+}
+
+//leer(__dirname + '/archivo.txt', console.log);
+
+function escribir(ruta, contenido, callback) {
+  fs.writeFile(ruta, contenido, function(error){
+    if(error){
+      console.error("No he podido escribirlo", error)
+    } else {
+      callback(contenido);
+    }
+  })
+}
+
+escribir(__dirname + '/archivo1.txt', 'HOLA SOY JAIME Y ESTO ES UNA PRUEBA', console.log);
+
+function borrar(ruta, callback) {
+  fs.unlink(ruta, callback);
+}
+
+borrar(__dirname + '/archivo1.txt', console.log);
---
+++ Escuela de Javascript/Fundamentos de NodeJs/modulos/globales.js
@@ -0,0 +1,3 @@
+//Tiene varios objetos dentro. Si es posible no usarlo porque genera muchos errores
+
+console.log(global)

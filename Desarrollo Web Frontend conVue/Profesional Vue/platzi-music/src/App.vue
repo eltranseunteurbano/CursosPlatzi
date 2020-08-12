498a1ce585aa2364e48feb2a001da9ac891f8250
Jaime Burbano <burbanojaime98@gmail.com>
2020-08-12 18:27:19 -0500

    setup con bugs vue

---
+++ Desarrollo Web Frontend conVue/Profesional Vue/platzi-music/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hola Vue!'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  @import "../../node_modules/bulma/css/bulma.css";
+
+</style>
